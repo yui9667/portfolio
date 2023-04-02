2817c80c139a5274f67eb50f3fe985abfe6f1208
yui9667 <109553017+yui9667@users.noreply.github.com>
2023-04-02 22:32:34 +0200

    Add files via upload

---
+++ Portfolio/index.js
@@ -0,0 +1,19 @@
+.socialmedia li{
+    justify-content: flex-start;
+   
+}
+.socialmedia li .icon1{
+    width: 2rem;
+    height: 2rem;
+object-fit: contain;
+position: relative;
+bottom: 2rem;
+}
+
+.socialmedia li .icon2{
+    width: 3rem;
+    height: 3rem;
+object-fit: contain;
+position: relative;
+bottom: 2rem;
+}
